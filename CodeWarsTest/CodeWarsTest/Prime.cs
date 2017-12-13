62e5d15fc72ca641f4244a563a2fb8585973c3be
drkrieger1 <eplyushko@gmail.com>
2017-12-12 19:21:18 -0800

    kata complete/ code wars site is timing out

---
+++ CodeWarsTest/CodeWarsTest/Prime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsTest
+{
+    class Prime
+    {
+        public static bool IsPrime(int n)
+        {
+            int num = 0;
+            if (n == 1)
+            {
+                return false;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    num++;
+                }
+            }
+
+            if (0 < num && num < 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+}
